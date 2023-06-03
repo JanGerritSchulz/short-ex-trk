6b730d0aa93b243a4e64e82c4086c51beab9a7a6
mmusich <marco.musich@cern.ch>
2023-06-03 15:16:28 +0200

    add MVA printouts

--- MyDirectory/PrintOutTracks/test/run_cfg_MVA.py
+++ MyDirectory/PrintOutTracks/test/run_cfg_MVA.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("RUN")
 
 process.source = cms.Source("PoolSource",
-                            fileNames = cms.untracked.vstring("file:/eos/user/c/cmsdas/2023/short-ex-trk/run321167_ZeroBias_AOD.root"))
+    fileNames = cms.untracked.vstring("file:/eos/user/c/cmsdas/2023/short-ex-trk/run321167_ZeroBias_AOD.root"))
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(5))
 
 process.MessageLogger = cms.Service("MessageLogger",
@@ -12,5 +12,5 @@ process.MessageLogger = cms.Service("MessageLogger",
 
 process.PrintOutTracks = cms.EDAnalyzer("PrintOutTracks")
 process.PrintOutTracks.tracks = cms.untracked.InputTag("generalTracks")
-
+process.PrintOutTracks.mvaValues = cms.untracked.InputTag("generalTracks","MVAValues")
 process.path = cms.Path(process.PrintOutTracks)
