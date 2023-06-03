f71e793c82ed93598f0e3f3da0a5d94307b1813d
mmusich <marco.musich@cern.ch>
2023-06-03 14:55:30 +0200

    add print out tracks directory

---
+++ MyDirectory/PrintOutTracks/scripts/printTracks.py
@@ -0,0 +1,15 @@
+import DataFormats.FWLite as fwlite
+events = fwlite.Events("/eos/user/c/cmsdas/2023/short-ex-trk/run321167_ZeroBias_AOD.root")
+tracks = fwlite.Handle("std::vector<reco::Track>")
+
+for i, event in enumerate(events):
+    if i >= 5: break # print info only about the first 5 events
+    print "Event", i
+    event.getByLabel("generalTracks", tracks)
+    for j, track in enumerate(tracks.product()):
+        print "    Track", j,
+        print "\t charge/pT: %.3f" %(track.charge()/track.pt()),
+        print "\t phi: %.3f" %track.phi(),
+        print "\t eta: %.3f" %track.eta(),
+        print "\t dxy: %.4f" %track.dxy(),
+        print "\t dz: %.4f"  %track.dz()
---
+++ MyDirectory/PrintOutTracks/test/run_cfg.py
@@ -0,0 +1,16 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("RUN")
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring("file:run321167_ZeroBias_AOD.root"))
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(5))
+
+process.MessageLogger = cms.Service("MessageLogger",
+    destinations = cms.untracked.vstring("cout"),
+    cout = cms.untracked.PSet(threshold = cms.untracked.string("ERROR")))
+
+process.PrintOutTracks = cms.EDAnalyzer("PrintOutTracks")
+process.PrintOutTracks.tracks = cms.untracked.InputTag("generalTracks")
+
+process.path = cms.Path(process.PrintOutTracks)
